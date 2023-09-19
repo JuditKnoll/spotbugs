6293c107f17f8bc65f9a8afdc0bc4b2aeb4f19fe
Judit Knoll <judit.knoll@sigmatechnology.com>
2023-09-19 12:47:23 +0200

    recreate issue

--- spotbugsTestCases/src/java/npe/DetectThrowers.java
+++ spotbugsTestCases/src/java/npe/DetectThrowers.java
@@ -6,10 +6,67 @@ public class DetectThrowers {
         throw new RuntimeException(s);
     }
 
-    public int falsePositive(Object x) {
+    private void oopsCaller(String s) {
+        oops(s);
+    }
+
+    private void ooops(String s) throws Exception {
+        throw new Exception(s);
+    }
+
+    private void ooopsCaller(String s) throws Exception {
+        ooops(s);
+    }
+
+    public int runtimeExInnerThrown(Object x) {
         if (x == null)
             oops("x is null");
         return x.hashCode();
     }
 
+    public int runtimeExInnerIndirectlyThrown(Object x) {
+        if (x == null)
+            oopsCaller("x is null");
+        return x.hashCode();
+    }
+
+    public int exInnerThrown(Object x) throws Exception {
+        if (x == null)
+            ooops("x is null");
+        return x.hashCode();
+    }
+
+    public int exInnerIndirectlyThrown(Object x) throws Exception {
+        if (x == null)
+            ooopsCaller("x is null");
+        return x.hashCode();
+    }
+
+    public int exOuterThrown(Object x) throws Exception {
+        if (x == null) {
+            DetectThrowersHelper.ooops("x is null");
+        }
+        return x.hashCode();
+    }
+
+    public int exOuterIndirectlyThrown(Object x) throws Exception {
+        if (x == null) {
+            DetectThrowersHelper.ooopsCaller("x is null");
+        }
+        return x.hashCode();
+    }
+
+    public int runtimeExOuterThrown(Object x) throws Exception {
+        if (x == null) {
+            DetectThrowersHelper.oops("x is null");
+        }
+        return x.hashCode();
+    }
+
+    public int runtimeExOuterIndirectlyThrown(Object x) throws Exception {
+        if (x == null) {
+            DetectThrowersHelper.oopsCaller("x is null");
+        }
+        return x.hashCode();
+    }
 }
