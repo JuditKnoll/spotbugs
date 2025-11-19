7c10a8ba0f0fe8b1aadf3278e18010cb1b94374c
Judit Knoll <knoll.judit94@gmail.com>
2025-11-19 15:16:12 +0100

    recreate connecting issue

--- spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue3769Test.java
+++ spotbugs-tests/src/test/java/edu/umd/cs/findbugs/detect/Issue3769Test.java
@@ -10,6 +10,9 @@ class Issue3769Test extends AbstractIntegrationTest {
         performAnalysis("ghIssues/Issue3769.class");
         assertBugInMethodAtLine("BC_IMPOSSIBLE_DOWNCAST","Issue3769", "issue3769", 12);
         assertBugInMethodAtLine("BC_IMPOSSIBLE_DOWNCAST","Issue3769", "issue3788", 16);
+        assertBugInMethodAtLine("BC_IMPOSSIBLE_DOWNCAST_OF_TOARRAY", "Issue3769", "issue3752", 23);
+        assertBugInMethodAtLine("BC_IMPOSSIBLE_DOWNCAST_OF_TOARRAY", "Issue3769", "issue3752", 24);
         assertBugTypeCount("BC_IMPOSSIBLE_DOWNCAST", 2);
+        assertBugTypeCount("BC_IMPOSSIBLE_DOWNCAST_OF_TOARRAY", 2);
     }
 }
--- spotbugsTestCases/src/java/ghIssues/Issue3769.java
+++ spotbugsTestCases/src/java/ghIssues/Issue3769.java
@@ -2,6 +2,7 @@ package ghIssues;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.ArrayList;
 
 /**
  * @see <a href="https://github.com/spotbugs/spotbugs/issues/3769">GitHub issue 3769</a> and <a href="https://github.com/spotbugs/spotbugs/issues/3788">GitHub issue 3788</a>
@@ -13,6 +14,12 @@ class Issue3769 {
     }
 
     public LinkedHashMap issue3788(HashMap map) {
-        return (LinkedHashMap) map; // should report a BC_IMPOSSIBLE_DOWNCAST warning, but no warnings
+        return (LinkedHashMap) map; // BC_IMPOSSIBLE_DOWNCAST
+    }
+
+    public void issue3752() {
+        ArrayList<String> a = new ArrayList<String>();
+        String s[] = (String[]) a.stream().toArray(); // BC_IMPOSSIBLE_DOWNCAST_OF_TOARRAY
+        String s2[] = (String[]) a.toArray(); // BC_IMPOSSIBLE_DOWNCAST_OF_TOARRAY
     }
 }
